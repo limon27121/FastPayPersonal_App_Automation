a716ed74eda4f091df7fc117cfdfa21e958483e0
limon27121 <moontaser009@gmail.com>
2026-04-01 18:10:01 +0600

    update send and request file also update the sh file and create the report

--- run_smoke.sh
+++ run_smoke.sh
@@ -2,18 +2,7 @@
 
 echo "🚀 Starting FastPay Smoke Test Suite..."
 
-maestro test \
-  -e User=7507507501 \
-  -e Password=Password100@1 \
-  -e Amount=261 \
-  -e Pin=5890 \
-  --format html \
-  --output smoke_report_final.html \
-  Flow/Smoke_Test/01_P0_Login.yaml \
-  Flow/Smoke_Test/02_P1_HomePage.yaml \
-  Flow/Smoke_Test/03_P2_WithdrawMoney.yaml \
-  Flow/Smoke_Test/04_P3_SendMoney.yaml \
-  Flow/Smoke_Test/05_P4_RequestMoney.yaml
+maestro test -e User=7507507501 -e Password=Password100@1 -e Amount=261 -e Pin=5890 --format html --output smoke.html Flow/Smoke_Test/*.yaml
 
 echo "✅ Tests complete! Opening report..."
-google-chrome smoke_report_final.html
+google-chrome smoke.html
